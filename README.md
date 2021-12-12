f1d7b662290c6d9eed9df716235ab3cd31de8bfd
Anjan Talatam <tspa2117@gmail.com>
2021-12-12 12:01:17 +0530

    2.Higher Order Functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### Functional Programming
+
+2.Higher Order Functions
---
+++ index.js
@@ -0,0 +1,7 @@
+const users = ["anjan", "talatam"];
+
+// forEach, map, reduce ..... are higher order functions
+users.forEach((user) => console.log(user));
+
+// another example of higher order function
+setTimeout(() => console.log("Hi"), 5000); // Hi will be consoled after  5 seconds
